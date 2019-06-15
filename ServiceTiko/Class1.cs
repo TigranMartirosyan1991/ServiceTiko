2d80c30e60a796210391b4162ec15c70325ae77a
Tigran# <Tigran#@LAPTOP-0CV90VBK>
2019-06-15 18:44:59 +0400

    Add project files.

---
+++ ServiceTiko/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceTiko
+{
+    public class Class1
+    {
+    }
+}
